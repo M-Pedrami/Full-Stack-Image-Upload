372321521d2e0d4847417c048bece69fdc4ec197
Maziar Pedrami <awildroot@gmail.com>
2023-12-04 11:04:12 +0100

    created FE and schemas/models

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const {Schema, model} = require("mongoose");
+const User = require("./user")
+
+const productSchema = new Schema({
+  name : {type : String, required: true},
+  price : {type : Number, required: true, min: 0},
+  image : {type: String, required: true},
+  owner : {type: Schema.Types.ObjectId, ref: "User"}
+});
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
